9137baaeb2ad054b0e369dbc371b6c9b2a169cd8
Franco Waisfeld <francowaisfeld@gmail.com>
2022-01-31 15:13:35 -0800

    Exercises for arrays chapter

---
+++ arrays/arrays7.rb
@@ -0,0 +1,3 @@
+numbers = [1, 231, 45, 234, 6, 78]
+
+numbers.each_with_index { |value, index| puts "index: #{index} value: #{value}" }
---
+++ arrays/arrays8.rb
@@ -0,0 +1,5 @@
+first_array = [1,2,3,4,5,6]
+
+second_array = first_array.map { |number| number + 2 }
+
+p first_array, second_arraypwd
