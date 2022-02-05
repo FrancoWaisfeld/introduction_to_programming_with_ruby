26903935b4bbbe06db92378c6ab53f0d936a86f1
Franco Waisfeld <francowaisfeld@gmail.com>
2022-02-04 17:00:46 -0800

    exercises16.rb

--- exercises/exercises16.rb
+++ exercises/exercises16.rb
@@ -1,10 +1,11 @@
 contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"], ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
 contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
-items = [:email, :address, :phone]
+data_types = [:email, :address, :phone]
 
-contacts.each_with_index do |(name, hash), id|
-  items.each do |item|
-    hash[item] = contact_data[id].shift
+contacts.each_with_index do |(contact, hash), index|
+  data_types.each do |data|
+    hash[data] = contact_data[index].shift
   end
 end
+
 p contacts
